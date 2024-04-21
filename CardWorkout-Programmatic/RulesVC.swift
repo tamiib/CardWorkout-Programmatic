133d138e58785e87bf215f49ece1f89c6b91f940
Tamara Barišić <tamarabarisic@MacBook-Pro-od-Tamara.local>
2024-04-21 13:22:12 +0200

    CardWorkout app

---
+++ CardWorkout-Programmatic/RulesVC.swift
@@ -0,0 +1,74 @@
+//
+//  RulesVC.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Tamara Barišić on 01.04.2024..
+//
+
+import UIKit
+
+class RulesVC: UIViewController {
+    
+    let titleLabel = UILabel()
+    let rulesLabel = UILabel()
+    let exerciseLabel = UILabel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground;
+        configureTitleLabel()
+        configureRulesLabel()
+      configureExerciseLabel()
+    }
+
+    func configureTitleLabel()
+    {
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false;
+        titleLabel.text="Rules"
+        titleLabel.font = .systemFont(ofSize: 40, weight: .bold)
+        titleLabel.textAlignment = .center
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor , constant: 60),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
+        ])
+    }
+    
+    func configureRulesLabel()
+    {
+        view.addSubview(rulesLabel)
+        rulesLabel.translatesAutoresizingMaskIntoConstraints = false;
+        
+        rulesLabel.text="The value od each card represents the number of exercise you do.\n\nJ = 11, Q = 12, Q = 13, K = 13, A = 14"
+        rulesLabel.textAlignment = .center
+        rulesLabel.font = .systemFont(ofSize: 19, weight: .semibold)
+        rulesLabel.lineBreakMode = .byWordWrapping
+        rulesLabel.numberOfLines = 0
+        
+        NSLayoutConstraint.activate([
+            rulesLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor , constant: 25),
+            rulesLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            rulesLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
+        ])
+    }
+    
+    
+    func configureExerciseLabel()
+    {
+        view.addSubview(exerciseLabel)
+        exerciseLabel.translatesAutoresizingMaskIntoConstraints = false;
+        
+        exerciseLabel.text = "♠️ = Push-ups\n\n❤️ = Sit-up\n\n♣️ = Burpees\n\n♦️ = Jumping Jackes"
+        exerciseLabel.textAlignment = .left
+        exerciseLabel.font = .systemFont(ofSize: 19, weight: .semibold)
+        exerciseLabel.numberOfLines = 0
+        
+        NSLayoutConstraint.activate([
+            exerciseLabel.topAnchor.constraint(equalTo: rulesLabel.bottomAnchor , constant: 60),
+            exerciseLabel.widthAnchor.constraint(equalToConstant: 200),
+            exerciseLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+}
